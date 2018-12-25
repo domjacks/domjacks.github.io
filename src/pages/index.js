f5a27f02b0d0959ada36b79e6dee7892a5c34254
Dom Jackson <dom.jackson@bbc.co.uk>
2018-12-25 01:02:00 +0000

    Added react-helmet to manage page meta data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+
 import Layout from "../components/Layout";
 import Bio from "../components/Bio";
 
 const IndexPage = () => (
-  <Layout>
-    <Bio />
-  </Layout>
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Dom Jackson</title>
+      <link rel="canonical" href="https://domjacks1.github.io/" />
+    </Helmet>
+    <Layout>
+      <Bio />
+    </Layout>
+  </div>
 );
 
 export default IndexPage;
