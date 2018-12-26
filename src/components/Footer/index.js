6997046663babddc747659b5c39daf5ed0272a2f
Dom Jackson <dom.jackson@bbc.co.uk>
2018-12-26 14:20:37 +0000

    Updated Footer to include Copyright and fixed wrapping on mobile

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,20 @@ import React from "react";
 
 import "./index.css";
 
+const year = new Date().getFullYear();
+
 const Footer = () => (
   <div className="Footer-main">
-    <span>
-      Made using <a href="https://www.gatsbyjs.org/">GatsbyJS</a>, built with{" "}
-      <a href="https://travis-ci.org/">Travis CI</a> and hosted on{" "}
-      <a href="https://pages.github.com/">Github Pages</a>.
-    </span>
+    <div className="Footer-child">
+      <p>&copy; Dom Jackson {year}</p>
+    </div>
+    <div className="Footer-child">
+      <p>
+        Made using <a href="https://www.gatsbyjs.org/">GatsbyJS</a>, built with{" "}
+        <a href="https://travis-ci.org/">Travis CI</a> and hosted on{" "}
+        <a href="https://pages.github.com/">Github Pages</a>.
+      </p>
+    </div>
   </div>
 );
 
