8907c5939585af2c1e9aa78c5decdcd1d609c8bb
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-23 23:58:53 +0100

    Switch to using gtag gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,13 @@
-const cfg = {
-  plugins: [`gatsby-plugin-react-helmet`]
-};
-
-if (process.env.CONTEXT === "production") {
-  const googleAnalyticsCfg = {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: "UA-146431921-1",
-      // Defines where to place the tracking script - `true` in the head and `false` in the body
-      head: true
+{
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: "UA-146431921-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true
+      }
     }
-  };
-  cfg.plugins.push(googleAnalyticsCfg);
+  ];
 }
-
-module.exports = cfg;
