f2a10a7d5b957fb98290d67930d3ea3ac44f813b
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-23 23:32:06 +0100

    Add config for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
-{
+const cfg = {
   plugins: [`gatsby-plugin-react-helmet`]
+};
+
+if (process.env.CONTEXT === "production") {
+  const googleAnalyticsCfg = {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-146431921-1",
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: true
+    }
+  };
+  cfg.plugins.push(googleAnalyticsCfg);
 }
+
+module.exports = cfg;
