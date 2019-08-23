3ef95b83503865c9e09ee63e1d3853e9f84d8d4b
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-23 23:58:26 +0100

    Changed image

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 
 const Bio = () => (
   <div className="Bio-main">
-    <img src="./avatar.png" alt="dom's avatar" className="Bio-avatar" />
+    <img src="./dom.jpeg" alt="dom's avatar" className="Bio-avatar" />
     <h1 className="Bio-h1">DOM JACKSON</h1>
     <h2>
       <span role="img" aria-label="lightning">
