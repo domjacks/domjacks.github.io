8e5d4c77199e9f4215435b0aeab275efe366b333
Dom Jackson <dom.jackson@bbc.co.uk>
2019-08-30 23:34:01 +0100

    Added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+
+const PageNotFound = () => (
+  <Layout>
+    <Card>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+    </Card>
+  </Layout>
+);
+
+export default PageNotFound;
