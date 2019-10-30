ca498b0bbb8eff55018742bfbea0389f72486fe3
Dom Jackson <dom.jackson@bbc.co.uk>
2019-10-30 22:25:46 +0000

    Cache some pages and serve in SW

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,18 @@
-console.log("Hello from sw.js");
+self.addEventListener("install", function(event) {
+  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open("app-cache").then(function(cache) {
+      return cache.addAll(["/", "/dom.jpeg"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
