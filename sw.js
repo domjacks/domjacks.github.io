bf195b3f94491def37f84beac5aa49a2b4d49764
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-03 14:35:50 +0000

    Deploy domjacks/domjacks.github.io to github.com/domjacks/domjacks.github.io.git:master

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
+const version = 2;
+
 self.addEventListener("install", function(event) {
   console.log("Service Worker Installed");
   event.waitUntil(
-    caches.open("app-cache").then(function(cache) {
+    caches.open(`app-cache-${version}`).then(function(cache) {
       return cache.addAll(["/", "/dom.jpeg"]);
     })
   );
