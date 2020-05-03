6b09b4cbd63e57bbe7cd8827c1c364e8fff4cc72
domjacks <0x444a@protonmail.com>
2020-05-03 18:45:11 +0100

    Fix manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Dom Jackson 2`,
+        name: `Dom Jackson`,
         short_name: `Dom`,
         start_url: `/`,
         background_color: `#ffa500`,
