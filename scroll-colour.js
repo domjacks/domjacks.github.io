82e69ec764616b0466c137164171e9a41590adaa
Dom Jackson <me@domjackson.xyz>
2021-12-04 13:44:57 +0000

    Change wheel event to scroll

--- scroll-colour.js
+++ scroll-colour.js
@@ -3,7 +3,7 @@ let ticking = false;
 function setRandomBackgroundColour() {
     document.querySelector('.layout-main').style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);}
 
-document.addEventListener('wheel', function(e) {
+document.addEventListener('scroll', function(e) {
   if (!ticking) {
     window.requestAnimationFrame(function() {
         setTimeout(function () {
