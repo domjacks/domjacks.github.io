d3a7c7d8adaae2310e6810a49927ee43b2e0b1a5
Dom Jackson <me@domjackson.xyz>
2022-06-25 23:38:24 +0100

    Remove console log

--- secretKode.js
+++ secretKode.js
@@ -6,7 +6,6 @@ window.addEventListener('keyup', e => {
     pressed.push(e.key);
     pressed.splice(-secretKode.length - 1, pressed.length - secretKode.length );
 
-    console.log(pressed);
     if(secretKode.join() === pressed.join()) {
         console.log('Wahey');
     }
