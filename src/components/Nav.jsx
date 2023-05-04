e09b94195af935f7e2ab5c6f617b04d71d0e49ca
Dom Jackson <me@domjackson.xyz>
2023-05-04 17:39:36 +0100

    Create nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,33 @@
+import { A } from "@solidjs/router";
+import styles from './Nav.module.css'
+
 function Nav() {
     return (
-        <h1>Home</h1>
+        <nav>
+            <ol>
+                <li>
+                    <A href="/" class={styles.link}>/home</A>
+                </li>
+                <li>
+                    <A href="/now" class={styles.link}>/now</A>
+                </li>
+                <li>
+                    <A href="/readme" class={styles.link}>/readme</A>
+                </li>
+                <li>
+                    <a
+                        class={styles.link}
+                        href="https://github.com/domjacks"
+                        target="_blank"
+                        rel="noopener noreferrer">/code</a>
+                </li>
+                <li>
+                    <a
+                        class={styles.link}
+                        href="mailto:hello@domjackson.xyz">/contact</a>
+                </li>
+            </ol>
+        </nav>
     )
 }
 
