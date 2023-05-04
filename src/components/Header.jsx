2f4879e511d334d9fb3daf23273f0cdf583c9bfa
Dom Jackson <me@domjackson.xyz>
2023-05-04 17:39:18 +0100

    Simplfiy header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,9 @@ import styles from './Header.module.css'
 function Header() {
     return (
         <div class={styles.header}>
-            <p>
-                <span role="img" aria-label="wave" class={styles.bioWave}>
-                    👋
-                </span>
-            </p>
             <h1>
                 Dom Jackson
             </h1>
-            <h2><span class={styles.blink}>&gt;</span> software engineer</h2>
             <Nav />
         </div>
     )
